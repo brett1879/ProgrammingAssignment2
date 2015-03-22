3e3a441f2d40dcd7193771d41fc0cb50f6d75ac0
brett1879 <brettforman@yahoo.com>
2015-03-22 14:07:06 -0600

    Updated file for assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions.
+## makeCacheMatrix creates a special "matrix" object that can cache its inverse.
+## cacheSolve returns a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function of x that uses a matrix as an input, and can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ##this is creating global variables where data can be cached and accessed later if avaliable
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns a matrix that is the inverse of 'x'
+## it first looks to see if there is a cached value for matrix (m) and runs solve() to inverse it if so
+## if there is not a cached value of m, it will look for a local version and run solve() on it to generate the inverse if needed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    m <- x$get()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(solve(m))
+    }
+    ##this "if" is looking to see if there is a global value of m avaliable.  if so, it will print the words "getting cached data" and use it with the solve() function to return an inverse
+    data <- x$get()
+    m <- solve(data)
+    x$setsolve(m)
+    m
+    ## if not, it is taking a local copy of m and using solve to print the inverse
+  }
